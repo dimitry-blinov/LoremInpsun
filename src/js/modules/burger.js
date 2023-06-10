a7373bd43763666bde650973de5ce717f586bd09
dmitryblinov <d.o.blinov@mail.ru>
2023-06-10 19:19:41 +0500

    добавил функционал для закрытия по клику на элемент в меню

--- src/js/modules/burger.js
+++ src/js/modules/burger.js
@@ -1,18 +1,28 @@
-import { disableBodyScroll, enableBodyScroll, clearAllBodyScrollLocks } from 'body-scroll-lock';
+import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock';
 
+const closeMenu = (menu, button) => {
+  menu.classList.remove('menu--active');
+  button.classList.remove('burger--active')
+}
 
-const toggle = (e) => {
+const toggleMenu = (e) => {
   if (e.target.matches('.js-burger')) {
     const button = e.target;
     const menu = document.querySelector('.menu');
+    const items = document.querySelectorAll('.menu__item');
     const menuActive = document.querySelector('.menu--active');
 
     menu.classList.toggle('menu--active');
     button.classList.toggle('burger--active')
+
+    items.forEach((item) => item.addEventListener('click', () => {
+      closeMenu(menu, button);
+      enableBodyScroll(menu);
+    }))
 
     if (!menuActive) disableBodyScroll(menu);
     else enableBodyScroll(menu);
   }
 }
 
-document.addEventListener('click', toggle);
+document.addEventListener('click', toggleMenu);
